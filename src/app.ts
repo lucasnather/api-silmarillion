4d0e78a8f692f9abd2342a73664848f409bfe7a6
lucasnather <natherlucas@gmail.com>
2024-06-17 15:57:29 -0400

    docs: add swagger UI

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,19 @@ import express from 'express'
 import { env } from './env/env.js'
 import { valarRouter } from './routes/valar-routes.js'
 import { app } from './server.js'
-
+import swaggerUIExpress from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 
 app.use(express.json())
 
 const port = env.PORT || 8080
+
+app.use(
+    "/docs",
+    swaggerUIExpress.serve,
+    swaggerUIExpress.setup(swaggerDocs, { explorer: true})
+);
+
 
 app.get('/', (request, response) => {
     response.json({
@@ -18,4 +26,5 @@ app.use(valarRouter)
 
 app.listen(port, () => {
     console.log(`Sever running at http://localhost:${port}`)
+    console.log(`Docs running at route http://localhost:${port}/docs`)
 })
